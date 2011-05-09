c61c2437981217b3db232e9f988410ddd7ba105f
jpearson <jpearson556@gmail.com>
2011-05-09 16:06:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaboApp::Application.config.secret_token = '8e8e443537c876fe3719f48287d78cf3e9ff0b518bbc2b2ac4d5b06d40a30ceff9faf44badf37275ad5b7c86bb574cf293f5d90a6ccac5d813f27f9e378bd205'
